fd021f69ca54c60c23afabcb723329d3f9da0d69
Aldo <acasareto@gmail.com>
2022-11-01 13:12:05 +0100

    post sent to backend

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,24 @@
 import '../styles/globals.css';
 import Layout from '../components/layout';
 import type { AppProps } from 'next/app';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <ToastContainer
+        position='top-center'
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='light'
+      />
       <Component {...pageProps} />
     </Layout>
   );
--- pages/post.tsx
+++ pages/post.tsx
@@ -1,16 +1,39 @@
 import React, { useEffect, useState } from 'react';
-import { auth } from '../utils/firebase';
+import { auth, db } from '../utils/firebase';
 import { useRouter } from 'next/router';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { toast } from 'react-toastify';
 
 const Post = () => {
-  const [message, setMessage] = useState<string>('');
+  const [message, setMessage] = useState({ description: '' });
+  const [user, loading] = useAuthState(auth);
 
-  const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+  const submitHandler = async (e: any) => {
     e.preventDefault();
-    console.log(e.target);
-    const { value } = e.target as HTMLButtonElement;
-    console.log(`value = `, value);
+
+    if (!message.description) {
+      toast.error('please add a message', {
+        position: 'top-center',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'light',
+      });
+    }
+
+    const collectionRef = collection(db, 'messages');
+    await addDoc(collectionRef, {
+      ...message,
+      timestamp: serverTimestamp(),
+      user: user?.uid,
+      avatar: user?.photoURL,
+      username: user?.displayName,
+    });
+    setMessage({ description: '' });
   };
 
   console.log(message);
@@ -22,15 +45,19 @@ const Post = () => {
         <div className='py-2'>
           <h3 className='text-lg font-medium py-2'>Description</h3>
           <textarea
-            value={message}
+            value={message.description}
             className='bg-gray-800 h-48 w-full text-white p-2 rounded-xl text-small'
-            onChange={(e) => setMessage(e.target.value)}
+            onChange={(e) => setMessage({ ...message, description: e.target.value })}
             name='textarea'
           />
-          <p className={` text-sm ${message.length > 300 ? 'text-red-600' : ''}`}>
-            {message.length}/300
+          <p className={` text-sm ${message.description.length > 300 ? 'text-red-600' : ''}`}>
+            {message.description.length}/300
           </p>
-          <button className='w-full bg-cyan-600 text-white rounded-lg p-2 my-2' type='submit'>
+          <button
+            disabled={message.description.length > 300 && true}
+            className='w-full bg-cyan-600 text-white rounded-lg p-2 my-2'
+            type='submit'
+          >
             Submit
           </button>
         </div>
